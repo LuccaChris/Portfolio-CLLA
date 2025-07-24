058bda35b1937ea755c68074f1cba00157de6759
Christian <112213507+LuccaChris@users.noreply.github.com>
2025-07-24 10:08:14 -0300

    Update AssemblyInfo for new build
    Updated AssemblyInformationalVersion and cache to reflect the latest build output.

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fed7b121a33593b30a3d83fcfea9bec2df0e700")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0708461d26352c1b2db45e902633cf3c45a9e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
