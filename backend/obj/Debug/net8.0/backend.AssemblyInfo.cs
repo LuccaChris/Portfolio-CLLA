9e351c95cc8114e5ae242bc5cae966757c4b9467
Christian <112213507+LuccaChris@users.noreply.github.com>
2025-07-24 10:09:21 -0300

    Atualizar versão informacional do assembly e cache de entradas

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0708461d26352c1b2db45e902633cf3c45a9e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+058bda35b1937ea755c68074f1cba00157de6759")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
